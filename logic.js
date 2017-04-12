855c376873df27caaa44dde15c0ddacf44ffbaf7
Sophia Dossland <sophia.dossland@gmail.com>
2017-04-11 18:24:12 -0700

    initiate functions: -add new item -write list to HTML

--- logic.js
+++ logic.js
@@ -3,4 +3,38 @@
  */
 document.addEventListener("DOMContentLoaded", function () {
 
+  const orderForm = document.querySelector('.addItem'),
+        orderedItemsList = document.querySelector('.orderedItems'),
+        allItems = [];
+
+  //places an order on submit
+  //1. create new item as an object called item
+  //2. push item into allItems list
+  //3. map newly revised list to display each item in HTML
+  //4. clear the order form after each submit
+  function addItem(e) {
+    e.preventDefault();
+    //'this' references the form element this event handler is run on (see second to last line)
+    const newItem = (this.querySelector('[name=item]')).value;
+    const item = {
+      newItem,
+      orderPlaced: false //delete this line??
+    };
+    allItems.push(item);
+    populateOrderedItemsList(allItems, orderedItemsList);
+    this.reset();
+  }
+
+  function populateOrderedItemsList(dishes = [], dishList) {
+    dishList.innerHTML = dishes.map(dish => {
+      return `
+        <li>
+            <label>${dish.newItem}</label>
+        </li>
+      `;
+    }).join(''); //removes commas between <li> elements
+  }
+
+  //run addEventListener method on form element since it triggers an event
+  orderForm.addEventListener('submit', addItem);
 });
